2a710eb144019082fa02f76d3a51a770da71bfdb
Abuthelezi <ST10466281@rcconnect.edu.za>
2025-03-29 13:35:32 +0200

    Adding some comments to the code

--- app/src/main/java/com/example/mymeal/MainActivity.kt
+++ app/src/main/java/com/example/mymeal/MainActivity.kt
@@ -28,42 +28,53 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-
+            //Declaring Variables
             var meal_type by remember {
                 mutableStateOf("")
             }
             var time_of_the_day by remember {
                 mutableStateOf("")
             }
-
+            //column to align items on the android app.
             Column(
+                //Center everything on the app
                 horizontalAlignment = Alignment.CenterHorizontally,
                 modifier = Modifier.fillMaxSize()
             ) {
+                //name of the app at the top.
                 Text("My Meal",
+                    //size of the app name
                 fontSize = 30.sp,
+                    //To make the app name bold
                     fontWeight = FontWeight.Bold
                 )
+                //The instruction what the user should type in the text box right above the textbox
                 Text("Plan your meal",
+                    //font size of the instruction text
                     fontSize = 30.sp,
                     fontWeight = FontWeight.Bold )
+                //the divider line between textbox and title
                 Divider()
+                //The spacing the items on the app.
                 Spacer(modifier = Modifier.size(30.dp))
-
-
+                //inserting the textbox on the app
                 OutlinedTextField(
+
                     value = time_of_the_day,
                     onValueChange = { text ->
                         time_of_the_day = text
                     },
+                    //This to show the instructions in the textbox.
                     placeholder = {
                         Text("Enter time of day:Morning;Mid-morning,Afternoon, Mid-afternoon or dinner")
                     }
 
                 )
                 Spacer(modifier = Modifier.size(30.dp))
+                //add buttons next to each other
                 Row {
                     Button(onClick = {
+                        //validation in enter button
                         meal_type = when(time_of_the_day){
                             "Morning" -> "Spread mashed avocado on a slice of whole-grain toast"
                             "Mid-morning" -> "Peanut Butter Banana Wraps"
